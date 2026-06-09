4f153ca27f521e9de05a8e358ee67efd55b147db
lasikavisalac <lasikac4@gmail.com>
2026-06-09 12:21:13 +0530

    PF
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml

---
+++ src/marks.java
@@ -0,0 +1,19 @@
+void main(){
+    int[] marks={87,44,89,32,23};
+    System.out.print("pass marks: ");
+    for (int i = 0; i < marks.length; i++){
+            if (marks[i] >= 40) {
+                System.out.print(marks[i] + " ,");
+            }
+              }
+                System.out.println();
+
+    System.out.print("Fail marks: ");
+    for( int i = 0; i<marks.length;i++){
+        if(marks[i] < 40){
+            System.out.print(marks[i] + " ,");
+
+        }
+    }
+
+}
