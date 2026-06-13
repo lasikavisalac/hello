53c91167719aea37e8f6a3ef99c8175a3cd56bec
lasika <lasikac4@gmail.com>
2026-06-13 12:35:26 +0530

    record

--- src/day8.java
+++ src/day8.java
@@ -1,3 +1,9 @@
 public class day8 {
-    
+     record CourseRecord( String course, String department){
+     }
+    static void main() {
+        CourseRecord courseRecord = new CourseRecord("java","Bsc AI&DS");
+        System.out.println(courseRecord.course());
+        System.out.println(courseRecord.department());
+    }
 }
